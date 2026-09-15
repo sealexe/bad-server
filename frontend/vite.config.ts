310cbd3e2a2346c98fc97033bf13d95fee6d268d
Ekaterina Semenova <ek.semenova94@gmail.com>
2026-09-15 14:48:56 +0300

    chore: обновлены vite, react-router-dom и зависимые плагины

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   css: {
     preprocessorOptions: {
       scss: {
+        loadPaths: [resolve(__dirname)],
         additionalData: `
           @use "./src/scss/variables" as *;
           @use "./src/scss/mixins";
